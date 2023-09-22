c18cc7a1bef86cf66468c6c859d79a63ec35c67f
shyynux <shyynux@gmail.com>
2023-09-22 12:58:15 +0530

    Add a new book

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -1,8 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const CreateBooks = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [status, setStatus] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSaveBook = () => {
+    const data = {
+      title,
+      author,
+      status,
+    };
+    setLoading(true);
+    axios
+      .post('http://localhost:5555/books/', data)
+      .then(() => {
+        setLoading(false);
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert('Enter all the fields, do not miss the status');
+        console.log(error);
+      });
+  };
+
   return (
-    <div>CreateBooks</div>
+    <div className='p-4'>
+    <BackButton />
+    <h1 className='text-3xl my-4'>Create Book </h1>
+    {loading ? <Spinner /> : '' }
+    <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+    <div className='my-4'>
+      <label className='text-xl mr-4 text-gray-500'>Title</label>
+      <input 
+        type='text'
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        className='border-2 border-gray-500 px-4 py-2 w-full'
+        />
+    </div>
+    <div className='my-4'>
+      <label className='text-xl mr-4 text-gray-500'>Author</label>
+      <input 
+        type='text'
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+        className='border-2 border-gray-500 px-4 py-2 w-full'
+        />
+    </div>
+      <div className='my-4'>
+      <label className='text-xl mr-4 text-gray-500'>Status</label>
+      <select
+        value={status}
+        onChange={(e) => setStatus(e.target.value)}
+        className='border-2 border-gray-500 px-4 py-2 w-full'
+      >
+        <option value='Current Read'>Current Read</option>
+        <option value='Completed'>Completed</option>
+        <option value='Dropped'>Dropped</option>
+        <option value='To Be Read'>To Be Read</option>
+      </select>
+  </div>
+    <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>
+      Save
+    </button>
+      </div>
+    </div>
   )
 }
 
